5b5a6d8792e0e67e32ca251f1f01d248f51d6a50
omkargarde <42486951+omkargarde@users.noreply.github.com>
2024-03-14 15:47:04 +0000

    project section done

--- src/lib/customComponent/Projects.svelte
+++ src/lib/customComponent/Projects.svelte
@@ -1,13 +1,51 @@
-<h2 id="projects" class="text-xl pt-12 text-blue-500">Projects</h2>
+<h2 id="projects" class="text-xl pt-4 text-blue-500">Projects</h2>
 
+<!-- project 1-->
 <div class="pt-4 text-lg">Ecommerce website using MERN</div>
 <div class="pt-2 text-balance">
 	Created an e-commerce website that enables seamless product purchasing for users and streamlined
 	product management for administrators.
 </div>
-<a href="https://github.com/omkargarde/mern-ecommerce" class="mt-2 hover:text-blue-500 underline"
-	>Github</a
-><br />
-<a href="https://proshop-yipu.onrender.com/" class="pt-2 hover:text-blue-500 underline"
-	>Demo
-</a><span class="text-red-500">(this loads slow because of free tier)</span>
+<div class="pt-2">
+	<a href="https://github.com/omkargarde/mern-ecommerce" class=" hover:text-blue-500 underline"
+		>Github</a
+	>
+</div>
+<div class="pt-2">
+	<a href="https://proshop-yipu.onrender.com/" class=" hover:text-blue-500 underline">Demo </a><span
+		class="text-red-500 pl-2">(this loads slow because of free tier)</span
+	>
+</div>
+
+<!-- project 2-->
+<div class="pt-4 text-lg">AngularAdminUI</div>
+<div class="pt-2 text-balance">
+	Developed the Angular admin UI project to provide a dynamic and interactive user interface for
+	managing user roles. Utilized Angular framework to create a seamless user experience, resulting in
+	improved efficiency and ease of use for administrators
+</div>
+<div class="pt-2">
+	<a href="https://github.com/omkargarde/angularAdminUI" class=" hover:text-blue-500 underline"
+		>Github</a
+	>
+</div>
+<div class="pt-2">
+	<a href="https://angular-admin-ui.vercel.app/" class=" hover:text-blue-500 underline">Demo </a>
+</div>
+
+<!-- project 3-->
+<div class="pt-4 text-lg">Events management using NextJS</div>
+<div class="pt-2 text-balance">
+	Developed events management using NextJS, React, and CSS modules. Streamlined event creation and
+	management by implementing features such as event registration, resulting in improved efficiency
+	and user satisfaction. Enhanced user experience by incorporating responsive design principles and
+	optimizing page load times for seamless navigation and faster performance.
+</div>
+<div class="pt-2">
+	<a href="https://github.com/omkargarde/nextJSEvent" class=" hover:text-blue-500 underline"
+		>Github</a
+	>
+</div>
+<div class="pt-2">
+	<a href="https://next-js-event-chi.vercel.app/" class=" hover:text-blue-500 underline">Demo </a>
+</div>
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -3,9 +3,9 @@
 	import '../app.pcss';
 </script>
 
-<div class=" bg-slate-50">
+<div class=" bg-slate-50 pb-8">
 	<Header />
-	<main class="mx-auto w-3/5 pt-12 text-gray-500">
+	<main class="mx-auto w-3/5 pt-12 text-gray-700">
 		<slot />
 	</main>
 </div>
