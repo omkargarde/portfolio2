dcd89c086041b63fd7d5fb485d68fe2d2133a478
omkargarde <42486951+omkargarde@users.noreply.github.com>
2024-03-15 17:46:21 +0530

    removed header section for now

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,11 +1,9 @@
 <script>
 	import Footer from '$lib/customComponent/Footer.svelte';
-	import Header from '$lib/customComponent/Header.svelte';
 	import '../app.pcss';
 </script>
 
 <div class="  pb-8">
-	<!-- <Header /> -->
 	<main class="sm:w-3/5 sm:mx-auto mx-8 w-11/12 pt-12 text-gray-700">
 		<slot />
 	</main>
