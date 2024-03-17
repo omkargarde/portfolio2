795e46d7925f3fa986f35d0ae35c4d8a38726b97
omkargarde <42486951+omkargarde@users.noreply.github.com>
2024-03-17 16:39:16 +0530

    added google analytics

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,9 +1,11 @@
 <script>
+	import Analytics from '$lib/customComponent/Analytics.svelte';
 	import Footer from '$lib/customComponent/Footer.svelte';
 	import '../app.pcss';
 </script>
 
-<div class="  pb-8">
+<Analytics />
+<div class="pb-8">
 	<main class="sm:w-3/5 mx-auto w-11/12 pt-12 text-gray-700">
 		<slot />
 	</main>
