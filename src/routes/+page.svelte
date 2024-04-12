989b5c5b6922dbc6201b174c3fa2f4724dfda438
omkargarde <42486951+omkargarde@users.noreply.github.com>
2024-04-12 18:35:33 +0530

    pivot to astro

--- src/routes/+page.svelte
+++
@@ -1,9 +0,0 @@
-<script>
-	import Greetings from '$lib/customComponent/Greetings.svelte';
-	import Projects from '$lib/customComponent/Projects.svelte';
-	import Skills from '$lib/customComponent/Skills.svelte';
-</script>
-
-<Greetings />
-<Skills />
-<Projects />
