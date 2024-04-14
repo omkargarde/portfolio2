6b425b8777c1fcb5502975888f4829ef0e6283c8
omkargarde <42486951+omkargarde@users.noreply.github.com>
2024-04-14 12:54:23 +0530

    Revert "warngler"
    This reverts commit 58133eb375b66c03efb02e79860622e87ecb4a00.

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,12 +2,8 @@ import { defineConfig } from "astro/config";
 import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
 import tailwind from "@astrojs/tailwind";
-import cloudflare from "@astrojs/cloudflare";
 
-// https://astro.build/config
 export default defineConfig({
   site: "https://portfolio2-b39.pages.dev",
   integrations: [mdx(), sitemap(), tailwind()],
-  output: "server",
-  adapter: cloudflare()
 });
