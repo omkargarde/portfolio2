2f34fc29819865dce4296692b1f2e707e5eec5d7
omkargarde <42486951+omkargarde@users.noreply.github.com>
2024-10-03 23:08:37 +0530

    header skeleton done

---
+++ src/components/Header.tsx
@@ -0,0 +1,16 @@
+const Header = () => {
+  return (
+    <header className="">
+      <nav className="">
+        <ul className="m-10 flex">
+          <li className="text-2xl">Omkar Garde</li>
+          <div className="flex-1"></div>
+          <li className="mx-2 text-lg">/Blog</li>
+          <li className="mx-2 text-lg">/Projects</li>
+        </ul>
+      </nav>
+    </header>
+  );
+};
+
+export default Header;
